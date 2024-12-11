e7d7f876a7d250265b95799c98e0c0ccbf3d59ad
Yonathan <yvbenitah@gmail.com>
2024-12-11 11:49:17 +0200

    Update README.md

--- README.js
+++ README.js
@@ -1,57 +1,114 @@
+eMerci pour la précision ! Voici une version du fichier .eslintrc.json correctement formatée et lisible 
+---
 
-// scripts/update-lib-deps.js
-const fs = require('fs');
-const path = require('path');
+Fichier .eslintrc.json corrigé et lisible
 
-// Lire le fichier de dépendances généré par nx graph
-const depsData = JSON.parse(fs.readFileSync('./deps.json', 'utf8'));
-
-// Pour chaque projet dans le graphe
-Object.keys(depsData.graph.nodes).forEach(projectName => {
-    const projectNode = depsData.graph.nodes[projectName];
-    
-    // Si c'est une librairie
-    if (projectNode.type === 'lib') {
-        const packageJsonPath = path.join('libs', projectName, 'package.json');
-        
-        // Lire le package.json existant
-        let packageJson = {};
-        try {
-            packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-        } catch (error) {
-            console.log(`Création nouveau package.json pour ${projectName}`);
-        }
-        
-        // Garder les champs existants importants
-        const existingFields = {
-            name: packageJson.name,
-            version: packageJson.version
-        };
-        
-        // Réinitialiser les dépendances
-        packageJson = {
-            ...existingFields,
-            dependencies: {},
-            peerDependencies: {}
-        };
-        
-        // Récupérer les dépendances du projet
-        const deps = Object.keys(depsData.graph.dependencies[projectName] || {});
-        
-        deps.forEach(dep => {
-            const depNode = depsData.graph.nodes[dep];
-            if (depNode) {
-                // Si c'est une dépendance interne (@a/*)
-                if (dep.startsWith('@a/')) {
-                    packageJson.dependencies[dep] = '*';
-                } else {
-                    packageJson.peerDependencies[dep] = '*';
-                }
-            }
-        });
-        
-        // Sauvegarder le package.json mis à jour
-        fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-        console.log(`Mis à jour: ${packageJsonPath}`);
+{
+  "root": true,
+  "ignorePatterns": ["node_modules", "dist", "tmp"],
+  "overrides": [
+    {
+      "files": ["*.ts", "*.tsx"],
+      "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "plugin:@nx/nx"
+      ],
+      "plugins": ["@typescript-eslint", "import"],
+      "parser": "@typescript-eslint/parser",
+      "parserOptions": {
+        "project": ["tsconfig.*?.json"]
+      },
+      "rules": {
+        "import/no-extraneous-dependencies": [
+          "error",
+          {
+            "devDependencies": [
+              "**/*.spec.ts",
+              "**/*.test.ts",
+              "jest.config.ts",
+              "karma.conf.js",
+              "webpack.config.js"
+            ]
+          }
+        ],
+        "import/order": [
+          "error",
+          {
+            "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
+            "newlines-between": "always"
+          }
+        ],
+        "import/no-cycle": ["error", { "maxDepth": 2 }],
+        "import/no-relative-parent-imports": "error",
+        "@typescript-eslint/no-unused-vars": ["error"],
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@nx/enforce-module-boundaries": [
+          "error",
+          {
+            "enforceBuildableLibDependency": true,
+            "allow": [],
+            "depConstraints": [
+              {
+                "sourceTag": "type:app",
+                "onlyDependOnLibsWithTags": ["type:feature", "type:shared"]
+              },
+              {
+                "sourceTag": "type:feature",
+                "onlyDependOnLibsWithTags": ["type:shared"]
+              }
+            ]
+          }
+        ]
+      }
+    },
+    {
+      "files": ["*.html"],
+      "extends": ["plugin:@angular-eslint/template/recommended"],
+      "rules": {}
+    },
+    {
+      "files": ["*.component.ts"],
+      "extends": ["plugin:@angular-eslint/recommended"],
+      "rules": {}
     }
-});
+  ]
+}
+
+
+---
+
+Clés pour rendre le fichier lisible :
+
+1. Indentation cohérente : Chaque niveau est aligné avec 2 espaces pour améliorer la lisibilité.
+
+
+2. Séparation claire :
+
+Les fichiers .ts, .html, et .component.ts sont bien isolés dans des sections spécifiques.
+
+
+
+3. Pas d'espaces inutiles ou de lignes superflues.
+
+
+
+
+---
+
+Si le format est encore incorrect :
+
+Installez et utilisez Prettier pour formater automatiquement ce fichier JSON :
+
+npm install --save-dev prettier eslint-config-prettier
+
+Puis exécutez :
+
+npx prettier --write .eslintrc.json
+
+
+Cela permettra de maintenir automatiquement un formatage propre. Dis-moi si ce fichier correspond à tes attentes !
+
